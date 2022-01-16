02d734a031a893ee4b3159e858c314c702881ece
Mizanali Panjwani <mizanalip786@gmail.com>
2022-01-17 01:17:22 +0530

    Setup React Native project

---
+++ mobile/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <ScrollView>
+        <Text>Hello World</Text>
+      </ScrollView>
+    </>
+  );
+};
+
+export default App;
